5b5b214cc7febc49ef352ea0647928c689b9a2ef
MatteoBarducci <mbardu1301@gmail.com>
2024-03-07 17:39:05 +0100

    feat: add color bonus

--- js/script.js
+++ js/script.js
@@ -7,24 +7,21 @@
 const container = document.querySelector('.container')
 // Creo il ciclo per la creazione delle boxes
 for (let i = 1; i <=100; i++){
-    let divBox = `<div class="box">${i}</div>`;
-    console.log (divBox);
-    // Metto il divBox dentro il container
-    container.innerHTML += divBox;
-
     let FizzBuzz;
     if (i % 3 === 0 & i % 5 === 0) {
-        let FizzBuzz = 'FizzBuzz';
+        FizzBuzz = 'FizzBuzz';
         console.log(FizzBuzz);
     } else if (i % 3 === 0) {
-        let FizzBuzz = 'Fizz';
+        FizzBuzz = 'Fizz';
         console.log(FizzBuzz);
     } else if (i % 5 === 0) {
-        let FizzBuzz = 'Buzz';
+        FizzBuzz = 'Buzz';
         console.log(FizzBuzz);
     } else {
-        let FizzBuzz = i;
+        FizzBuzz = i;
         console.log(FizzBuzz);
     }
-    
+    // Metto il divBox dentro il container
+    let divBox = `<div class="box ${FizzBuzz}">${FizzBuzz}</div>`;
+    container.innerHTML += divBox;
 }
